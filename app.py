9b4a888cc1fc61784506a8ce82b50991c8d92886
grlwithgrl <47872515+grlwithgrl@users.noreply.github.com>
2023-06-02 10:41:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -39,3 +39,37 @@ st.dataframe(df)
 
 # Display descriptive statistics of 'ages'
 st.write(df['ages'].describe())
+
+import pandas as pd
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Load the iris dataset
+iris = datasets.load_iris()
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+# Display the DataFrame in Streamlit
+st.dataframe(df)
+
+# Show general information about the dataset
+st.text(df.info())
+
+# Show statistical information about the dataset
+st.write(df.describe())
+
+# Select a feature to display histogram
+feature = st.selectbox('Select a feature', df.columns)
+
+# Plot histogram
+fig, ax = plt.subplots()
+ax.hist(df[feature], bins=20)
+
+# Set the title and labels
+ax.set_title(f'Histogram of {feature}')
+ax.set_xlabel(feature)
+ax.set_ylabel('Frequency')
+
+# Display the plot
+st.pyplot(fig)
+
